45eb10c42d56b3bb6e48c1ca1da29a99dd7bfa25
MatheusGrego <matheusdeleongrego@gmail.com>
2023-02-22 11:57:43 -0300

    init

---
+++ src/main/java/com/msuploadimg/MsUploadImgApplication.java
@@ -0,0 +1,13 @@
+package com.msuploadimg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsUploadImgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsUploadImgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msuploadimg/MsUploadImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msuploadimg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsUploadImgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
