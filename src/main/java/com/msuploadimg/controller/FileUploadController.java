1084e913e17d4f17d9eb702bf94598bb25a73879
MatheusGrego <matheusdeleongrego@gmail.com>
2023-02-22 16:11:39 -0300

    refactor: ControllerAdvice e handlers

--- src/main/java/com/msuploadimg/controller/FileUploadController.java
+++ src/main/java/com/msuploadimg/controller/FileUploadController.java
@@ -3,15 +3,15 @@ package com.msuploadimg.controller;
 import org.apache.tika.Tika;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 
+
 @RestController
-public class ImageUploadController {
+public class FileUploadController {
 
     @PostMapping("/upload-img")
     public ResponseEntity<String> uploadImage(@RequestPart("image") MultipartFile[] images) {
@@ -25,7 +25,6 @@ public class ImageUploadController {
             if (image.getSize() > 1048576) {
                 throw new MaxUploadSizeExceededException(1048576);
             }
-
 
             // Validando tipo mime da imagem
             String mimeType = new Tika().detect(image.getBytes());
@@ -42,8 +41,5 @@ public class ImageUploadController {
         }
     }
 
-    @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public ResponseEntity<String> handlerTamanhoMaximo(MaxUploadSizeExceededException e) {
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body("Permitimos imagens até " + e.getMaxUploadSize() + " bytes!");
-    }
+
 }
